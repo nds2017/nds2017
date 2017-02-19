691ee1aa8ebe3404bf24161b5565dc496d8f6d3e
nds2017 <nds.nds.2017@gmail.com>
2017-02-19 02:29:28 -0500

    Update rsync.sh

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -2,7 +2,7 @@
 # RSYNC.SH CODDEPLOY
 rsync -avz --delete-after --exclude-from=/tmp/deploy-current/scripts/deploy.exclude /tmp/deploy-current/* /var/www/nds2018/
 chmod -R 775 /var/www/nds2018
-chown -R www-data:www-data /var/www/html/nds2018
+chown -R www-data:www-data /var/www/nds2018
 #cd /dataweb/libero/3.libero.pe
 #aws s3 sync --profile default --region us-east-1  /dataweb/libero/3.libero.pe/public/ s3://libero-media/public/
 #composer update
